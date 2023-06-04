622ca54d6ebaa965e898a428276937e0431a4a95
HaroldMontanoHurtado <harold.montano@correounivalle.edu.co>
2023-06-04 13:28:24 -0500

    Cambio de todo el tablero a canvas

---
+++ src/chessEngine.py
@@ -0,0 +1,16 @@
+class GameState():
+    def __init__(self):
+        self.piezas = [
+            ['torre-negra','caballo-negro','alfil-negro','dama-negra',
+            'rey-negro','alfil-negro','caballo-negro','torre-negra'],
+            ['peon-negro','peon-negro','peon-negro','peon-negro',
+            'peon-negro','peon-negro','peon-negro','peon-negro'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
+            ['peon-blanco','peon-blanco','peon-blanco','peon-blanco',
+            'peon-blanco','peon-blanco','peon-blanco','peon-blanco'],
+            ['torre-blanca','caballo-blanco','alfil-blanco','dama-blanca',
+            'rey-blanco','alfil-blanco','caballo-blanco','torre-blanca']
+        ]
--- src/gui.py
+++ src/gui.py
@@ -1,15 +1,17 @@
+from chessEngine import GameState
 from customtkinter import CTk, CTkLabel, CTkButton
-from PIL import ImageTk, Image
-from numpy import *
+from tkinter import Canvas, PhotoImage
 
 class Window(CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.gs = GameState()
+        
         # variables creadas para centrar la ventana al iniciar el programa
         self.wtotal = self.winfo_screenwidth()
         self.htotal = self.winfo_screenheight()
-        self.wventana = 900
-        self.hventana = 740
+        self.wventana = 720
+        self.hventana = 760
         self.pwidth = round(self.wtotal/2-self.wventana/2)
         self.pheight = round(self.htotal/2-self.hventana/2)
         self.geometry(
@@ -18,47 +20,83 @@ class Window(CTk):
         self.title('Mad Chess - Artificial Intelligence')
         self.resizable(0,0)
         
-        self.images()
+        self.posXChessboard = 50
+        self.posYChessboard = 70
+        self.imagenes = {}
+        
+        self.interfaz = Canvas(self)
+        self.interfaz.place(x=self.posXChessboard,y=self.posYChessboard,
+                            width=640,height=640)
+        self.drawChessboard()
+        self.loadPieces()
+        self.showPieces()
+        self.numberLabels()
+        self.buttons()
 
-    def images(self):
-        tablero_img = ImageTk.PhotoImage(
-            Image.open('img/tablero.png'))
-        # piezas blancas
-        rey_blanco_img = ImageTk.PhotoImage(
-            Image.open('img/rey-blanco.png'))
-        dama_blanca_img = ImageTk.PhotoImage(
-            Image.open('img/dama-blanca.png'))
-        alfil_blanco_img = ImageTk.PhotoImage(
-            Image.open('img/alfil-blanco.png'))
-        caballo_blanco_img = ImageTk.PhotoImage(
-            Image.open('img/caballo-blanco.png'))
-        torre_blanca_img = ImageTk.PhotoImage(
-            Image.open('img/torre-blanca.png'))
-        peon_blanco_img = ImageTk.PhotoImage(
-            Image.open('img/peon-blanco.png'))
-        # piezas negras
-        rey_negro_img = ImageTk.PhotoImage(
-            Image.open('img/rey-negro.png'))
-        dama_negra_img = ImageTk.PhotoImage(
-            Image.open('img/dama-negra.png'))
-        alfil_negro_img = ImageTk.PhotoImage(
-            Image.open('img/alfil-negro.png'))
-        caballo_negro_img = ImageTk.PhotoImage(
-            Image.open('img/caballo-negro.png'))
-        torre_negra_img = ImageTk.PhotoImage(
-            Image.open('img/torre-negra.png'))
-        peon_negro_img = ImageTk.PhotoImage(
-            Image.open('img/peon-negro.png'))
+    # Everything of the images
+    def drawChessboard(self):
+        #self.interfaz.create_rectangle(x0,y0,x1,y1,fill='')
+        tam_lado=80
+        for i in range(8):
+            for j in range(8):
+                if (i+j)%2==0:
+                    self.interfaz.create_rectangle(
+                        i*tam_lado,j*tam_lado,
+                        (i+1)*tam_lado,(j+1)*tam_lado,fill='#bac8d3')
+                else:
+                    self.interfaz.create_rectangle(
+                        i*tam_lado,j*tam_lado,
+                        (i+1)*tam_lado,(j+1)*tam_lado,fill='#18141d')
+
+    def loadPieces(self):
+        piezas = [
+            'rey-blanco','dama-blanca','alfil-blanco','caballo-blanco','torre-blanca','peon-blanco',
+            'rey-negro','dama-negra','alfil-negro','caballo-negro','torre-negra','peon-negro']
+        for pieza in piezas:
+            self.imagenes[pieza] = PhotoImage(file='./img/' + pieza + '.png')
+        #self.interfaz.create_image(40,40,image=self.imagenes['torre-blanca'],anchor='nw')
+
+    def showPieces(self):
+        tam_lado=80
+        for index_i, i in enumerate(self.gs.piezas):
+            for index_j, j in enumerate(i):
+                if j != '--':
+                    self.interfaz.create_image(
+                        index_j*tam_lado, index_i*tam_lado+1, image=self.imagenes[j], anchor='nw')
+    # ------------------------
+
+    def numberLabels(self):
+        for i in range(8):
+            CTkLabel(
+                master=self, text=f'{8-i}', fg_color="transparent",
+                width=40, height=30, font=('Comic Sans MS', 25)).place(x=5,y=80*i+95)
+        letters=['A','B','C','D','E','F','G','H']
+        c=0
+        for j in letters:
+            CTkLabel(
+                master=self, text=f'{letters[c]}', fg_color="transparent",
+                width=40, height=30, font=('Comic Sans MS', 25)).place(x=70+80*c,y=715)
+            c+=1
+
+    def buttons(self):
+        # initialization
+        btn_newGame = CTkButton(
+            master=self, text="Start new game", #command=funcion_button_BFS,
+            width=160, height=40, #border_width=0, state='disabled', text_color_disabled='white', corner_radius=8, 
+            font=('Comic Sans MS', 20))
+        btn_return = CTkButton(
+            master=self, text="Return play", #command=funcion_button_BFS,
+            width=160, height=40, #border_width=0, state='disabled', text_color_disabled='white', corner_radius=8, 
+            font=('Comic Sans MS', 20))
         
-        CTkLabel(master=self, text='', image=tablero_img).place(x=160,y=10)
+        btn_Resign = CTkButton( # ni idea pa que lo creé
+            master=self, text="Start new game", #command=funcion_button_BFS,
+            width=160, height=40, #border_width=0, state='disabled', text_color_disabled='white', corner_radius=8, 
+            font=('Comic Sans MS', 18))
         
-        def tablero_inicial():
-            posY = 51
-            posX = 201
-            # piezas negras arriba
-            CTkLabel(master=self, text='', image=torre_negra_img).place(x=posX,y=posY)
-        
-        tablero_inicial()
+        # instantiate
+        btn_newGame.place(x=50, y=10)
+        btn_return.place(x=290, y=10)
 
 if __name__=="__main__":
     window = Window()
